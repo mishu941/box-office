fe16d864993479d8e00e6cb82644d14a66a33d48
Mishu Jain <mishujain920@gmail.com>
2022-07-29 23:06:07 +0530

    styled show page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+/* eslint-disable */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
 
+import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route>
-          <div>Not Found</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ const Home = () => {
   const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption === 'shows';
-
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
